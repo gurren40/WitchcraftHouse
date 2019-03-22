af1d64d9290ae4e7d6ec0c432dfc9f2bb90f57f5
Aditiya Sidabutar <sidabutar.not.end@gmail.com>
2019-03-23 02:06:01 +0700

    bisa jadi background process, tapi tidak seperti yang diharapkan

--- AndroidControlDevice/android/src/id/web/witchcraft/house/MyService.java
+++ AndroidControlDevice/android/src/id/web/witchcraft/house/MyService.java
@@ -2,10 +2,12 @@ package id.web.witchcraft.house;
 import org.qtproject.qt5.android.bindings.QtService;
 import android.content.Context;
 import android.content.Intent;
-
+import android.app.PendingIntent;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.app.AlarmManager;
+import android.os.SystemClock;
 
 public class MyService extends QtService{
     private static NotificationManager m_notificationManager;
@@ -26,10 +28,35 @@ public class MyService extends QtService{
             m_notificationManager = (NotificationManager)m_instance.getSystemService(MyService.NOTIFICATION_SERVICE);
         }
 
+        Intent notificationIntent = new Intent(m_instance, MyActivity.class);
+        //notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(m_instance, 0, notificationIntent, 0);
+
         m_builder = new Notification.Builder(m_instance);
         m_builder.setSmallIcon(R.drawable.icon);
         m_builder.setContentTitle(title);
         m_builder.setContentText(body);
+        m_builder.setContentIntent(pendingIntent);
+        //m_builder.flags |= Notification.FLAG_AUTO_CANCEL;
         m_notificationManager.notify(id, m_builder.build());
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sendBroadcast(new Intent("WitchcraftServiceNeedRestart"));
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onDestroy();
+        sendBroadcast(new Intent("WitchcraftServiceNeedRestart"));
+    }
+
 }
