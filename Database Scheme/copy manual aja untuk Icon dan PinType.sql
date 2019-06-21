5491f39d35c023bc10c7143913a18dcb5571d48e
gurren40 <sidabutar.not.end@gmail.com>
2019-06-22 04:46:00 +0700

    tambah manual tabel isian Icon dan PinType

---
+++ Database Scheme/copy manual aja untuk Icon dan PinType.sql
@@ -0,0 +1,40 @@
+-- insert icons;
+
+INSERT INTO Icon (iconID,iconName) VALUES
+('0','default'),
+('1','house'),
+('2','lamp'),
+('3','switch'),
+('4','outlet'),
+('5','fan'),
+('6','thermometer'),
+('7','lock'),
+('8','key'),
+('9','fire'),
+('10','AC'),
+('11','cctv'),
+('12','bell'),
+('13','sun'),
+('14','moon'),
+('15','thermostat'),
+('16','power'),
+('17','door'),
+('18','water'),
+('19','leaf');
+
+-- insert pinTypes
+
+INSERT INTO PinType (pinTypeID,pinTypeName) VALUES
+('0','default'),
+('1','switch'),
+('2','textout'),
+('3','tempc'),
+('4','textfield'),
+('5','textarea'),
+('6','tumbler'),
+('7','spinbox'),
+('8','combobox'),
+('9','webview'),
+('10','rangeslider'),
+('11','colordialog'),
+('12','slider');
